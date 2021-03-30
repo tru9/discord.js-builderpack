09e4ae87f46a5243975b50bf05608bd1668e3137
tru9 <71397571+tru9@users.noreply.github.com>
2021-03-29 20:04:35 -0600

    Delete build directory

--- build/Events/ready.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-module.exports = {
-    type: "on",
-    eventName: "ready",
-    execute: function (client) {
-        var _a;
-        console.log(new Date().toLocaleTimeString() + " - " + ((_a = client.user) === null || _a === void 0 ? void 0 : _a.username) + " is online!");
-    }
-};
